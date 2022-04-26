67a9440cc4333d925f9244a8020d5a7f394944ef
DariaShmeleva <dgshmeleva@mail.ru>
2022-04-26 16:05:07 +0300

    simple spring bot

---
+++ src/main/kotlin/ru/tinkoff/service/NewsBotService.kt
@@ -0,0 +1,64 @@
+package ru.tinkoff.service
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import org.telegram.telegrambots.bots.TelegramLongPollingBot
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.objects.Update
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow
+
+@Service
+class NewsBotService: TelegramLongPollingBot() {
+
+    @Value("\${telegram.botName}")
+    private val botName: String = ""
+
+    @Value("\${telegram.token}")
+    private val token: String = ""
+
+    override fun getBotUsername(): String = botName
+
+    override fun getBotToken(): String = token
+
+    override fun onUpdateReceived(update: Update) {
+        if (update.hasMessage()) {
+            val message = update.message
+            val chatId = message.chatId
+            val responseText = if (message.hasText()) {
+                val messageText = message.text
+                when {
+                    messageText == "/start" -> "Добро пожаловать!"
+                    messageText.startsWith("Кнопка ") -> "Вы нажали кнопку"
+                    else -> "Вы написали: *$messageText*"
+                }
+            } else {
+                "Я понимаю только текст"
+            }
+            sendNotification(chatId, responseText)
+        }
+    }
+
+    fun sendNotification(chatId: Long, responseText: String) {
+        val responseMessage = SendMessage(chatId.toString(), responseText)
+        responseMessage.enableMarkdown(true)
+        // добавляем 4 кнопки
+        responseMessage.replyMarkup = getReplyMarkup(
+            listOf(
+                listOf("Кнопка 1", "Кнопка 2"),
+                listOf("Кнопка 3", "Кнопка 4")
+            )
+        )
+        execute(responseMessage)
+    }
+
+    fun getReplyMarkup(allButtons: List<List<String>>): ReplyKeyboardMarkup {
+        val markup = ReplyKeyboardMarkup()
+        markup.keyboard = allButtons.map { rowButtons ->
+            val row = KeyboardRow()
+            rowButtons.forEach { rowButton -> row.add(rowButton) }
+            row
+        }
+        return markup
+    }
+}
---
+++ src/main/kotlin/ru/tinkoff/utils/ExtendedFunctions.kt
@@ -0,0 +1,7 @@
+package ru.tinkoff.utils
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow
+
+class ExtendedFunctions
